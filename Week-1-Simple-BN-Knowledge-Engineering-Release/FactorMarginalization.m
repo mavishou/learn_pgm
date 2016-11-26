957d23830aab8dda988559db13b98d169790191c
mavishou <mavishou82@gmail.com>
2016-11-26 15:30:25 +0800

    week1(ans3): factor marginalization

--- Week-1-Simple-BN-Knowledge-Engineering-Release/FactorMarginalization.m
+++ Week-1-Simple-BN-Knowledge-Engineering-Release/FactorMarginalization.m
@@ -38,6 +38,9 @@ indxB = AssignmentToIndex(assignments(:, mapB), B.card);
 % YOUR CODE HERE
 % Correctly populate the factor values of B
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
+indxA = 1:length(A.val);
+for i = 1:length(B.val)
+    B.val(i) = sum(A.val(indxA(indxB == i)))
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
